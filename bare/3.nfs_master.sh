b7237c29880ad15ed61e56ac31bdf8216f5d280a
Aurelio Antonio <aurelio.vivas@correounivalle.edu.co>
2018-09-12 17:50:31 -0500

    Update 3.nfs_master.sh

--- bare/3.nfs_master.sh
+++ bare/3.nfs_master.sh
@@ -2,12 +2,11 @@
 
 # Install the nfs server package
 apt-get install -y nfs-kernel-server
-# Change working dir to te home directory
-cd 
 # Creating the shared directory
 mkdir /home/mpiuser/cloud
 # Indicating the directory that will be shared
-sed -e '/home/mpiuser/cloud *(rw,sync,no_root_squash,no_subtree_check)' -ibak /etc/exports
+# sed -e '/home/mpiuser/cloud *(rw,sync,no_root_squash,no_subtree_check)' -ibak /etc/exports
+echo '/home/mpiuser/cloud *(rw,sync,no_root_squash,no_subtree_check)' >> /etc/exports
 # Exporting shared directories
 exportfs -a
 # Restarting the NFS server
